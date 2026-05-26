15c274a52a124303d253c99dcbc6feab3a604dfe
Ernesto505 <ernesto_mwc@hotmail.com>
2026-05-26 15:24:44 +0300

    Removed some events that are not needed

--- Assets/EventSystem/Events/GameStateEvent.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-using ContainmentProtocol.Game.Events;
-
-[CreateAssetMenu(fileName = "New Game State Event",
-    menuName = "Containment Protocol/Events/Game State Event")]
-public class GameStateEvent : BaseGameEvent<GameStateData> { }
--- Assets/EventSystem/UnityEvents/UnityGameStateEvent.cs
+++
@@ -1,4 +0,0 @@
-using UnityEngine.Events;
-using ContainmentProtocol.Game.Events;
-
-[System.Serializable] public class UnityGameStateEvent : UnityEvent<GameStateData> { }
